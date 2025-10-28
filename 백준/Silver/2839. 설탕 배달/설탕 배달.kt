b57982c38874ae52a5fb849c433f23633d4aff44
권동휘 <hocci0222@kakao.com>
2025-10-28 09:19:54 +0900

    ✏️ Fix: 알고리즘 설명 추가.

--- 백준/Silver/2839. 설탕 배달/설탕 배달.kt
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.kt
@@ -2,10 +2,12 @@ fun main() {
     val n = readln().toInt()
     var result = -1
     var temp = n
-    
+
+    //5kg 봉지를 최대한 많이 사용하는 방식으로
     var fiveCount = temp / 5
     
     while (fiveCount >= 0) {
+        //5키로 전부 쓴 후 남는 무게.
         val remaining = temp - (fiveCount * 5)
         
         if (remaining % 3 == 0) {
@@ -13,9 +15,11 @@ fun main() {
             result = fiveCount + threeCount
             break
         }
-        
+        //반복문 돌 때마다 5키로 하나씩 줄이고 재시도
         fiveCount--
     }
-    
+    /**
+     * 5Kg 봉지 개수를 줄여가며 모든 조합을 시도하는 방식으로
+     */
     println(result)
 }
