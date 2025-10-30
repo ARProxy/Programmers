e2b75575de8eb3cbd59a3df2aa5121c4d9239681
Donghwi Kwon <131675760+ARProxy@users.noreply.github.com>
2025-10-30 09:19:41 +0900

    [Gold V] Title: 회의실 배정, Time: 1056 ms, Memory: 129268 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val time = readln().toInt()
+    val meetingClass = Array(time) {
+        readln().split(" ").map { it.toInt() }.toIntArray()
+    }
+    
+    meetingClass.sortWith(compareBy(
+        { it[1] }, { it[0] }
+    ))
+    
+    var count = 0
+    var lastEndTime = 0
+    
+    meetingClass.forEach { meeting ->
+        if (meeting[0] >= lastEndTime) {
+            count++
+            lastEndTime = meeting[1]
+        } 
+    }
+    
+    println(count)
+}
