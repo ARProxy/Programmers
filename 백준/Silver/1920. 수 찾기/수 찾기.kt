a2aae565f4f8ed26dcd55f9f40a8538228b86ee8
Donghwi Kwon <131675760+ARProxy@users.noreply.github.com>
2025-11-06 08:23:08 +0900

    [Silver IV] Title: 수 찾기, Time: 716 ms, Memory: 74876 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.kt
+++ 백준/Silver/1920. 수 찾기/수 찾기.kt
@@ -1,9 +1,11 @@
 fun main() {
     val n = readln().toInt()
-    val nSet = readln().split(" ").map { it.toInt() }.toSet()
+    val nValues = readln().split(" ").map { it.toInt() }.toSet()
     val m = readln().toInt()
-    
-    readln().split(" ").map { it.toInt() }.forEach {
-        println( if (it in nSet) 1 else 0 )
-    }
+    val mValues = readln().split(" ").map { it.toInt() }
+
+    println(mValues
+        .joinToString("\n") {
+            if (it in nValues) "1" else "0"
+    })
 }
