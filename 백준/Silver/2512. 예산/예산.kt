9ecc3fa104b068f37b0e74a108af2fe64e38c5f4
Donghwi Kwon <131675760+ARProxy@users.noreply.github.com>
2025-11-07 09:18:30 +0900

    [Silver II] Title: 예산, Time: 200 ms, Memory: 28076 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.kt
+++ 백준/Silver/2512. 예산/예산.kt
@@ -1,21 +1,26 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().toInt()
+    val budget = readLine().split(" ").map { it.toInt() }.toIntArray()
+    val sum = readLine().toInt()
     
-    val n = br.readLine().toInt()
-    val budget = br.readLine().split(" ").map { it.toInt() }.toIntArray()
-    val sum = br.readLine().toInt()
+    var left = 0
+    var right = budget.maxOf { it }
+    var result = 0
     
-    var upperLimit = budget.maxOf { it }
-    
-    while (true) {
-        val compareSum = budget.sumOf { it.coerceAtMost(upperLimit) }
+    while (left <= right) {
+        val mid = (left + right) / 2
+        val compareSum = budget.sumOf { it.coerceAtMost(mid).toLong() }
         
-        if (compareSum <= sum) break
-        upperLimit--
+        if (compareSum <= sum) {
+            result = mid
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
     }
     
-    println(upperLimit)
+    println(result)
 }
