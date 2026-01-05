b739fabe9a1cebfc30ed819329ec00076dc94750
Donghwi Kwon <131675760+ARProxy@users.noreply.github.com>
2026-01-05 10:11:32 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 576 ms, Memory: 53320 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        int[] prefix = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            sb.append(prefix[end] - prefix[start - 1]).append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+}
