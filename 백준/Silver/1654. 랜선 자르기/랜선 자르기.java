5598095e391a210ef20ed480574e480246058817
Donghwi Kwon <131675760+ARProxy@users.noreply.github.com>
2026-02-18 23:21:30 +0900

    [Silver II] Title: 랜선 자르기, Time: 156 ms, Memory: 17360 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,40 +1,36 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] firstLine = br.readLine().split(" ");
-        int k = Integer.parseInt(firstLine[0]);
-        int n = Integer.parseInt(firstLine[1]);
-        
-        int[] cables = new int[k];
-        for (int i = 0; i < k; i++) {
-            cables[i] = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        long[] lan = new long[K];
+        long max = 0;
+        for (int i = 0; i < K; i++) {
+            lan[i] = Long.parseLong(br.readLine().trim());
+            if (lan[i] > max) max = lan[i];
         }
-        
-        long left = 1;
-        long right = Arrays.stream(cables).max().orElse(0);
-        long maxLength = 0;
-        
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            long count = 0;
-            
-            for (int cable : cables) {
-                count += cable / mid;
-            }
-            
-            if (count >= n) {
-                maxLength = mid;
-                left = mid + 1;
+
+        long lo = 1, hi = max, ans = 0;
+
+        while (lo <= hi) {
+            long mid = (lo + hi) / 2;
+            long cnt = 0;
+            for (int i = 0; i < K; i++) cnt += lan[i] / mid;
+
+            if (cnt >= N) {
+                ans = mid;
+                lo = mid + 1;
             } else {
-                right = mid - 1;
+                hi = mid - 1;
             }
         }
-        
-        System.out.println(maxLength);
+
+        System.out.println(ans);
     }
 }
